1d686e9c40cf4be67db688176c6982bb1b9843b9
DenysVodzyk <vodzyk@gmail.com>
2021-07-04 21:44:40 -0400

    Solution to String Reverse

---
+++ src/StringReverse.java
@@ -0,0 +1,20 @@
+
+public class StringReverse {
+
+    public static void main(String[] args) {
+        reverseString("Hellok".toCharArray());
+    }
+
+    public static void reverseString(char[] s) {
+        char start;
+        char end;
+
+        for (int i = 0; i < s.length / 2; i++) {
+            start = s[s.length - i - 1];
+            end = s[i];
+            s[i] = start;
+            s[s.length - i - 1] = end;
+            i++;
+        }
+    }
+}
